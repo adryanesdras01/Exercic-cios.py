4291c04c5ec51c25220f96c0b230f3af4cebd0d4
adryanesdras01 <adryanesdras01@gmail.com>
2026-07-29 14:50:41 -0300

    Update Ex06.py

--- Ex06.py
+++ Ex06.py
@@ -1,3 +1,3 @@
 n =int(input('Digite um número: '))
 print(f'O dobro desse número é {n*2}, o triplo desse número é {n*3}')
-print('A raíz quadrada desse número é {n**1/2:.2f}.')
+print(f'A raíz quadrada desse número é {n**0.5:.2f}.')
