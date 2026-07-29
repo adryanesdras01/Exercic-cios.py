6f58ecab4130fe2b62c5fc1d8d37a4a4d5089b56
adryanesdras01 <adryanesdras01@gmail.com>
2026-07-29 15:30:19 -0300

    Update Ex20.py

--- Ex20.py
+++ Ex20.py
@@ -1,15 +1,10 @@
 import random
 
-alunos = []
-
-while True:
-    nome = input('Digite o nome do aluno ou digite (sair) para sair: ')
-    if nome.lower() == 'sair':
-        break
-    alunos.append(nome)
-
-if alunos:
-    random.shuffle(alunos)
-    print(f'A ordem de alunos fica {alunos}')
-else:
-    print('Nenhum aluno foi cadastrado')
+a = input('Digite o nome do Primeiro aluno: ')
+b = input('Digite o nome do Segundo aluno: ')
+c = input('Digite o nome do Terceiro aluno: ')
+d = input('Digite o nome do Quarto aluno: ')
+lista = [a, b, c, d]
+random.shuffle(lista)
+print(f'A ordem de apresentação será: ')
+print(f'{lista}')
