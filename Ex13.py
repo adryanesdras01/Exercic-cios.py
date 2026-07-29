a06e12f75561ce05afc889695a469e42069bbff0
adryanesdras01 <adryanesdras01@gmail.com>
2026-07-29 11:12:05 -0300

    Rename ex13.py to Ex13.py

---
+++ Ex13.py
@@ -0,0 +1,3 @@
+s = float(input('Qual o salário do funcionário?: R$'))
+a = s * 15/100
+print(f'O salário do funcionário com um aumento de 15% será de {s + a:.2f}')
--- ex13.py
+++
@@ -1,3 +0,0 @@
-s = float(input('Qual o salário do funcionário?: R$'))
-a = s * 15/100
-print(f'O salário do funcionário com um aumento de 15% será de {s + a:.2f}')
