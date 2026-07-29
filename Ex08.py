68d3d6a8effe035ab9ea4d3593d76bad80efc05b
adryanesdras01 <adryanesdras01@gmail.com>
2026-07-29 14:53:16 -0300

    Update Ex08.py

--- Ex08.py
+++ Ex08.py
@@ -1,2 +1,2 @@
 m = float(input('Qual o valor em metros?:  '))
-print('O valor em centímetros é {} e o valor em milímetros é {}'.format((m*100), (m*10000)))
+print(f'O valor em centímetros é {m*100} cm e o valor em milímetros é {m*10000} mm'.)
